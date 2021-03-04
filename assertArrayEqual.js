5ff327e33cff4cdd58ae8d010762a8e54598cf78
TrevBwz <tbowesy@hotmail.com>
2021-03-04 05:16:45 +0000

    end of day3 updates

--- assertArrayEqual.js
+++ assertArrayEqual.js
@@ -25,7 +25,7 @@ if (eqArrays(array1, array2)) {
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]) // => true
 
-ssertArraysEqual([1, 2, 3], [3, 2, 1]) // => false
+assertArraysEqual([1, 2, 3], [3, 2, 1]) // => false
 
-ssertArraysEqual(["1", "2", "3"], ["1", "2", "3"]) // => true
-ssertArraysEqual(["1", "2", "3"], ["1", "2", 3]) // => false
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]) // => true
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]) // => false
---
+++ findKeyByValue.js
@@ -0,0 +1,32 @@
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+const findByKeyValue = function(obj, value) {
+  let result = undefined;
+  for (let [key, searchValue] of Object.entries(obj)) {
+    if(value === searchValue) {
+    result = key;
+    }
+  }
+
+  return result;
+}
+
+
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+assertEqual(findByKeyValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findByKeyValue(bestTVShowsByGenre, "That '70s Show"), undefined);
