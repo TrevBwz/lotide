4909d67d911e53d016e1895f35c0835816045bdf
TrevBwz <tbowesy@hotmail.com>
2021-03-05 04:11:56 +0000

    end of d4

---
+++ findKey.js
@@ -0,0 +1,31 @@
+
+const findKey = function(obj, cb) {
+    for (let i in obj) {
+    if (cb(obj[i])) {
+    return i;
+    }
+  }
+  }
+ 
+
+const result = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual("noma", result);
