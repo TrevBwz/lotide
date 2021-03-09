b6315b3bcc1021aa6ee4b00dfa98caa49b9599fc
TrevBwz <tbowesy@hotmail.com>
2021-03-09 01:07:21 +0000

    index update

--- index.js
+++ index.js
@@ -20,22 +20,22 @@ const without = require('./without');
 
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle,
-  assertArraysEqual: assertArraysEqual,
-  assertEqual: assertEqual,
-  assertObjectsEqual:  assertObjectsEqual,
-  countLetters: countLetters,
-  countOnly: countOnly,
-  eqArrays:  eqArrays,
-  eqObjects: eqObjects,
-  findKey: findKey,
-  findByKeyValue: findKeyByValue,
-  letterPositions: letterPositions,
-  map: map,
-  takeUntil: takeUntil,
-  without: without,
+  head,
+  tail,
+  middle,
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  letterPositions,
+  map,
+  takeUntil,
+  without
 
 
 };
